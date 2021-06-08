66cd75bbd964fad4cf7fd4667faa475f332da9f1
Erik Huang <ekhu94@gmail.com>
2021-06-08 07:11:19 -0700

    adds own implementations for two examples of rank and suit

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,13 @@
-RSpec.describe do
-    it 'has a type' do
-        card = Card.new('Ace of Spades')
-        expect(card.type).to eq('Ace of Spades')
-    end
+require_relative '../lib/card'
+
+RSpec.describe Card do
+  card = Card.new('Ace', 'Spades')
+
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
 end
