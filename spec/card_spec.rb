0dfedc50f02bb71c578f7ef5ee08b173d2c72a9a
Erik Huang <ekhu94@gmail.com>
2021-06-08 07:47:13 -0700

    adds before hook to define instance variable of card

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/card'
 
 RSpec.describe Card do
-  card = Card.new('Ace', 'Spades')
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
 
   it 'has a rank' do
-    expect(card.rank).to eq('Ace')
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(card.suit).to eq('Spades')
+    expect(@card.suit).to eq('Spades')
   end
 end
