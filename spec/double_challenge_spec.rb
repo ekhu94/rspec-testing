5636a94e6420a785835b76c3d5cbc62cce08e2a7
Erik Huang <ekhu94@gmail.com>
2021-06-28 09:06:06 -0700

    creates own implementation for doubles challenge

---
+++ spec/double_challenge_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'doubles' do
+
+  it "tests the student's knowledge of the course content" do
+
+    db = double('Database Connection', connect: true, disconnect: 'Goodbye')
+    expect(db.connect).to be true
+    expect(db.disconnect).to eq('Goodbye')
+
+    fs = double('File System')
+    allow(fs).to receive_messages(read: 'Romeo and Juliet', write: false)
+    expect(fs.read).to eq('Romeo and Juliet')
+    expect(fs.write).to be false
+    
+  end
+
+end
