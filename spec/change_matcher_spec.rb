90c818ab4bfa5a8e86fe16082b8242f2f6954315
Erik Huang <ekhu94@gmail.com>
2021-07-04 16:41:59 -0700

    changes in positive and negative vals

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe 'change matcher' do
+  
   subject { ['a', 'b', 'c'] }
 
-  it 'checks for changes to an object' do
-    expect { subject.push('d') }.to change { subject.length }.from(3).to(4)
+  it 'checks for positive changes to an object' do
+    expect { subject.push('d') }.to change { subject.length }.by(1)
   end
+
+  it 'checks for negative changes to an object' do
+    expect { subject.pop }.to change { subject.length }.by(-1)
+  end
+
 end
