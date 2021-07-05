b7b451e0c563fe3804e9de8dc729a1e22fb3da03
Erik Huang <ekhu94@gmail.com>
2021-07-05 14:36:31 -0700

    contain_exactly ignore order

--- spec/contain_exactly_spec.rb
+++ spec/contain_exactly_spec.rb
@@ -1,8 +1,10 @@
 RSpec.describe 'contain_exactly matchers' do
 
-  subject { ['Edelgard', 'Dimitri', 'Claude'] }
+  subject { ['Edelgard', 'Dimitri'] }
 
   it 'checks all values contained in an array regardless of order' do
+    expect(subject).to contain_exactly('Dimitri', 'Edelgard')
+    subject.push('Claude')
     expect(subject).to contain_exactly('Claude', 'Dimitri', 'Edelgard')
   end
 
