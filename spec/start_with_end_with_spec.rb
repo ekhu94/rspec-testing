8206110db78c8c64a405ab1bcaa871f4421d59bc
Erik Huang <ekhu94@gmail.com>
2021-07-05 14:59:50 -0700

    add string assertions

--- spec/start_with_end_with_spec.rb
+++ spec/start_with_end_with_spec.rb
@@ -3,10 +3,12 @@ RSpec.describe 'start_with and end_with matchers' do
   it 'checks the beginning of an array or string' do
     expect([10, 20, 30, 40, 50]).to start_with(10, 20)
     expect(['Ruby', 'Python', 'Java']).to start_with('Ruby')
+    expect('I before E except after C').to start_with('I before')
   end
 
   it 'checks the end of an array or string' do
     expect([60, 70, 80, 90, 100]).to end_with(80, 90, 100)
     expect(['HTML', 'CSS', 'Javascript']).to end_with('Javascript')
+    expect('I before E except after C').to end_with('after C')
   end
 end
